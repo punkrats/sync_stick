050c39c071f6bdc1674169a75a0254af17921da1
André Pankratz <andre@vidibus.com>
2020-09-23 02:19:13 +0200

    Improve documentation

--- sync_stick.rb
+++ sync_stick.rb
@@ -2,14 +2,14 @@
 
 # Copies files of a local folder to a MP3 stick in order.
 #
-# The stick will play files in order that they have been copied. So when
-# anything within a folder changes, all contents of that folder have to be
-# copied again. This script will backup and restore files to avoid the slow
-# copying.
-
+# Many MP3 sticks will play files in order that they have been copied. So
+# when anything within a folder changes, all contents of that folder have
+# to be copied again. This script will use internal backup and restore to
+# avoid copying, which is slow.
+#
 # Usage:
 # ./sync_stick.rb <source folder> [<destination folder>]
-
+#
 # Example
 # ./sync_stick.rb ~/Stick/ /Volumes/STICK
 
